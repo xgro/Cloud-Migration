0c23f912b2f4e2e4604498d8fecd72a2c8874078
xgro <zero1401@naver.com>
2022-08-09 18:30:20 +0900

    Squashed 'product/' changes from 7db4b03..97c97a2
    97c97a2 Feat: update product api
    93eb225 Fix: amend slim to alpine
    
    git-subtree-dir: product
    git-subtree-split: 97c97a27d00fdeec1eec57d9945b42dd79182360

--- routes/product/index.js
+++ routes/product/index.js
@@ -1,29 +1,22 @@
 'use strict'
 
-// const products = [
-//   {id: 1, name: '2021년 달력', price: 6000, description: '최-신 2021년 달력'},
-//   {id: 2, name: '외계어 번역기', price: 12000, description: '화성어과 금성어간의 통역이 가능합니다'},
-// ]
-
 module.exports = async function (fastify, opts) {
-  fastify.decorate("authenticate", async function(request, reply){
-    try {
-      await request.jwtVerify()
-    } catch (err) {
-      reply.code(401).send(err)
+  fastify.get('/', async (request, reply) => {
+      let data;
+      const params = {
+        TableName: "product"
+      };
+
+      try {
+        data = await fastify.dynamo.scan(params).promise();
+        return { data }
+      } catch (e) {
+        reply.send(e)
+      }
     }
-  })
+  )
 
-  // fastify.get('/', {
-  //   // onRequest:[fastify.authenticate]
-  // }, async function (request, reply) {
-  //   reply.send(products)
-  // })
-
-  fastify.get('/:id', {
-    onRequest:[fastify.authenticate]
-  }, 
-  async (request, reply) => {
+  fastify.get('/:id', async (request, reply) => {
       let data;
       const { id } = request.params;
 
@@ -43,9 +36,7 @@ module.exports = async function (fastify, opts) {
     }
   )
 
-  fastify.post('/', {
-    onRequest:[fastify.authenticate]
-  }, async function (request, reply) {
+  fastify.post('/', async function (request, reply) {
     const {id, name, price, description} = request.body
 
     const newItem = {
